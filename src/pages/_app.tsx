4df8d443940da6feb2015d67f70043bb34ada34b
gsbenevides2 <gsbenevides2@gmail.com>
2021-01-25 21:31:06 +0000

    feat:add progress indicator

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,13 @@ import { ThemeProvider } from 'styled-components'
 import GlobalStyle from '../styles/global'
 import theme from '../styles/theme'
 import '../utils/firebase'
+import Router from 'next/router'
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
+
+Router.events.on('routeChangeStart', () => NProgress.start())
+Router.events.on('routeChangeComplete', () => NProgress.done())
+Router.events.on('routeChangeError', () => NProgress.done())
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
