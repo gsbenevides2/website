51776b5da3572d97979910633f2a3455853093ae
gsbenevides2 <gsbenevides2@gmail.com>
2021-02-07 13:06:31 +0000

    feat:list posts using firebase

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -3,8 +3,39 @@ import Header from '../../components/blog/Header'
 import { PageContainer } from '../../styles/pages/BlogHome'
 import Link from 'next/link'
 import Head from 'next/head'
+import { GetStaticProps, InferGetStaticPropsType } from 'next'
+import firebase from '../../utils/firebase'
 
-const BlogIndexPage: React.FC = () => {
+interface Post {
+  name: string
+  id: string
+  image: string
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const collectionSnapshot = await firebase
+    .firestore()
+    .collection('postsOfBlog')
+    .get()
+  const posts: Post[] = collectionSnapshot.docs.map(doc => {
+    return {
+      name: doc.data().name,
+      image: doc.data().image,
+      id: doc.id
+    }
+  })
+  return {
+    props: { posts },
+    revalidate: 60
+  }
+}
+
+const BlogIndexPage: React.FC<InferGetStaticPropsType<
+  typeof getStaticProps
+>> = props => {
+  const firstPost = props.posts[0]
+  const postA = props.posts.slice(1, 4)
+  const morePosts = props.posts.slice(5)
   return (
     <React.Fragment>
       <Head>
@@ -12,22 +43,28 @@ const BlogIndexPage: React.FC = () => {
       </Head>
       <Header />
       <PageContainer>
-        <Link href="/blog/post/criar-sites-com-react-e-ssr">
+        <Link href={`/blog/post/${firstPost.id}`}>
           <li className="firstPost">
-            <img src="https://picsum.photos/300/200" />
-            <h2>Como desenvolver aplicações performaticas com next</h2>
+            <img src={firstPost.image} />
+            <h2>{firstPost.name}</h2>
           </li>
         </Link>
-        <Link href="/blog/post/criar-sites-com-react-e-cu">
-          <li className="post postA">
-            <img src="https://picsum.photos/300/200" />
-            <h2>Como desenvolver aplicações performaticas com next</h2>
-          </li>
-        </Link>
-        <li className="post postMore">
-          <img src="https://picsum.photos/300/200" />
-          <h2>Como desenvolver aplicações performaticas com next</h2>
-        </li>
+        {postA.map(post => (
+          <Link key={post.id} href={`/blog/post/${post.id}`}>
+            <li className="post postA">
+              <img src={post.image} />
+              <h2>{post.name}</h2>
+            </li>
+          </Link>
+        ))}
+        {morePosts.map(post => (
+          <Link href={`/blog/post/${post.id}`}>
+            <li className="post postMore">
+              <img src={post.image} />
+              <h2>{post.name}</h2>
+            </li>
+          </Link>
+        ))}
       </PageContainer>
     </React.Fragment>
   )
