834e487d80a9e910ef9815f34349e9103ecb29be
gsbenevides2 <gsbenevides2@gmail.com>
2021-02-14 13:31:40 +0000

    feat: bundler analyzer

--- next.config.js
+++ next.config.js
@@ -1,5 +1,11 @@
 const withImages = require('next-images')
 
-module.exports = withImages({
-  esModule: true,
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true'
 })
+
+module.exports = withBundleAnalyzer(
+  withImages({
+    esModule: true
+  })
+)
