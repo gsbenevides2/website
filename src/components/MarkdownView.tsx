bda5e291194ed2754ad656edc56f0e591d607d93
gsbenevides2 <gsbenevides2@gmail.com>
2021-02-19 22:56:18 +0000

    feat:copy code to clipboard

--- src/components/MarkdownView.tsx
+++ src/components/MarkdownView.tsx
@@ -2,6 +2,27 @@ import React from 'react'
 import ReactMarkdown from 'react-markdown'
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
 import { dracula } from 'react-syntax-highlighter/dist/cjs/styles/prism'
+import styled from 'styled-components'
+import { FiCopy } from 'react-icons/fi'
+
+const CopyToClipabord = styled.span`
+  display: inline-block;
+  background-color: #282a36;
+  color: #f8f8f2;
+  font-family: Consolas, Monaco, 'Andale Mono', monospace;
+  line-height: 1.5;
+  margin-top: -12.8px;
+  width: 100%;
+  text-align: center;
+  padding: 16px;
+  border-top: 1px solid #f8f8f2;
+  margin-bottom: 8px;
+  border-radius: 0px 0px 4.8px 4.8px;
+  transition: 0.2s;
+  &:hover {
+    background-color: #44475a;
+  }
+`
 
 interface MarkdownViewProps {
   text: string
@@ -11,10 +32,22 @@ interface CodeBlockProps {
   value: string
 }
 const CodeBlock: React.FC<CodeBlockProps> = ({ language, value }) => {
+  function copyToClipabord() {
+    if ('clipboard' in navigator) {
+      navigator.clipboard.writeText(value)
+    } else {
+      alert('Não foi possivel acessar a Area de Transferencia')
+    }
+  }
   return (
-    <SyntaxHighlighter language={language} style={dracula}>
-      {value}
-    </SyntaxHighlighter>
+    <>
+      <SyntaxHighlighter language={language} style={dracula}>
+        {value}
+      </SyntaxHighlighter>
+      <CopyToClipabord onClick={copyToClipabord}>
+        <FiCopy /> &emsp; Copiar para Area de Transferencia
+      </CopyToClipabord>
+    </>
   )
 }
 const MarkdownView: React.FC<MarkdownViewProps> = ({ text }) => {
