8c6c3b90c9b31458293287f058b09167cc85db94
Guilherme da Silva Benevides <gsbenevides2@gmail.com>
2021-04-30 22:28:34 +0000

    fix ngrok error

--- src/pages/blog/postExemple.tsx
+++ src/pages/blog/postExemple.tsx
@@ -5,7 +5,7 @@ import PostPage, { ServerSideProps } from './post/[id]'
 export const getServerSideProps: GetServerSideProps<ServerSideProps> = async context => {
   const { postId, ngrokId } = context.query
   const postResponse = await fetch(
-    `https://${ngrokId}.ngotk.io/props?postId=${postId}`
+    `https://${ngrokId}.ngrok.io/props?postId=${postId}`
   )
   const post = await postResponse.json()
   return { props: { post } }
