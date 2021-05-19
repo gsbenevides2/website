b19c284f680efaa2b8a3b6f1bb3eb44cbba42d7e
Guilherme da Silva Benevides <gsbenevides2@gmail.com>
2021-05-19 02:11:37 +0000

    fix base64

--- src/utils/firebase/config.ts
+++ src/utils/firebase/config.ts
@@ -1,19 +1,8 @@
 import { FirebaseOptions } from 'firebase/app'
-
+import { decode } from 'js-base64'
 export function getConfig(): FirebaseOptions {
-  let firebaseConfig: FirebaseOptions
-
-  try {
-    firebaseConfig = JSON.parse(
-      atob(process.env.NEXT_PUBLIC_FIREBASE_CREDENTIALS)
-    )
-  } catch (e) {
-    firebaseConfig = JSON.parse(
-      Buffer.from(
-        process.env.NEXT_PUBLIC_FIREBASE_CREDENTIALS,
-        'base64'
-      ).toString('utf8')
-    )
-  }
+  const firebaseConfig: FirebaseOptions = JSON.parse(
+    decode(process.env.NEXT_PUBLIC_FIREBASE_CREDENTIALS)
+  )
   return firebaseConfig
 }
