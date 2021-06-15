b3a0a4b78530b187cf9034a9082eba883b164ded
Guilherme da Silva Benevides <gsbenevides2@gmail.com>
2021-06-15 00:39:30 +0000

    fix: layout bugs

--- src/components/WelcomeModal.tsx
+++ src/components/WelcomeModal.tsx
@@ -69,12 +69,14 @@ export const WelcomeModal: React.FC = () => {
   const container = React.useRef<HTMLDivElement>(null)
   const close = React.useCallback(() => {
     setTimeout(() => {
+      document.body.style.overflow = 'none'
       container.current.classList.remove('show')
     }, 2000)
   }, [container])
   React.useEffect(() => {
     const firstAccess = localStorage.getItem('firstAccess_v2')
     if (!firstAccess) {
+      document.body.style.overflow = 'hidden'
       container.current.classList.add('show')
       localStorage.setItem('firstAccess_v2', Date.now().toString())
     }
@@ -89,10 +91,10 @@ export const WelcomeModal: React.FC = () => {
           src={WelcomeBlog}
         />
         <p>
-          Beleza! Meu nome é Guilherme e é um prazer imenso ter você aqui
-          comigo, seja muito bem vindo. Nesse espaço publicarei conteúdo sobre
-          minha vida, programação e tudo que eu achar relevante vai estar aqui.
-          Desejo uma otima leitura. Ao continuar você concorda com os termos{' '}
+          Olá! Meu nome é Guilherme e é um prazer imenso ter você aqui comigo,
+          seja muito bem vindo. Nesse espaço publicarei conteúdo sobre minha
+          vida, programação e tudo que eu achar relevante vai estar aqui. Desejo
+          uma otima leitura. Ao continuar você concorda com os termos{' '}
           <a href="/politica" target="_blank">
             aqui
           </a>
