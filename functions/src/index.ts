1553293a240f575a94c75d8eca793109c5beadb5
Guilherme da Silva Benevides <gsbenevides2@gmail.com>
2023-12-02 15:50:52 -0300

    feat: add functions, rules and setup repo

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,19 +1,42 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * import {onCall} from "firebase-functions/v2/https";
- * import {onDocumentWritten} from "firebase-functions/v2/firestore";
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
-
-import {onRequest} from "firebase-functions/v2/https";
 import * as logger from "firebase-functions/logger";
+import {onDocumentDeleted} from "firebase-functions/v2/firestore";
+import * as admin from "firebase-admin";
 
-// Start writing functions
-// https://firebase.google.com/docs/functions/typescript
+admin.initializeApp();
 
-// export const helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+/**
+ * Deletes a storage folder.
+ * @param {string} path - The path of the folder to delete.
+ * @return {Promise<void>}
+ */
+async function deleteStorageFolder(path: string): Promise<void> {
+  const bucket = admin.storage().bucket();
+  await bucket.deleteFiles({prefix: path});
+}
+
+export const deletedBlogPost = onDocumentDeleted(
+  "posts/{blogPostId}",
+  async (event) => {
+    logger.info("Deleted post", {structuredData: true});
+    const blogPostId = event.params.blogPostId;
+    await deleteStorageFolder(`posts/${blogPostId}`);
+  }
+);
+
+export const deleteCertificate = onDocumentDeleted(
+  "certifications/{certificateId}",
+  async (event) => {
+    logger.info("Deleted certificate", {structuredData: true});
+    const certificateId = event.params.certificateId;
+    await deleteStorageFolder(`certifications/${certificateId}`);
+  }
+);
+
+export const deleteProject = onDocumentDeleted(
+  "projects/{projectId}",
+  async (event) => {
+    logger.info("Deleted project", {structuredData: true});
+    const projectId = event.params.projectId;
+    await deleteStorageFolder(`projects/${projectId}`);
+  }
+);
