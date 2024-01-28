9b79c77f2f390402f767c61d6542c4fae70155f1
Guilherme da Silva Benevides <gsbenevides2@gmail.com>
2024-01-28 11:04:08 -0300

    fix: some issues

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,23 +1,23 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
-import { getLink } from "./services/firebase/admin/links";
 
 // This function can be marked `async` if using `await` inside
 export async function middleware(request: NextRequest) {
   if (request.nextUrl.pathname.startsWith("/l/")) {
     const id = request.nextUrl.pathname.replace("/l/", "");
-    try {
-      const link = await getLink(id);
-      if (!link) return NextResponse.redirect(new URL("/404", request.url));
-      return NextResponse.redirect(link.url);
-    } catch (e: any) {
-      NextResponse.redirect(new URL("/500", request.url));
-    }
+    const redUrl =
+      process.env.NODE_ENV === "development"
+        ? "http://localhost:5001/gui-dev-br/us-central1/links"
+        : "htttps://links.functions.gui.dev.br";
+    const origem =
+      process.env.NODE_ENV === "development"
+        ? "http://localhost:3000"
+        : "https://gui.dev.br";
+    const makeUrl = `${redUrl}/${id}?origem=${origem}`;
+    return NextResponse.redirect(makeUrl);
   }
 }
-
 // See "Matching Paths" below to learn more
 export const config = {
   matcher: "/l/:id*",
 };
-
