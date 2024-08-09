26fa2c94c0c399976e33a57658c8895c351a5f00
Guilherme da Silva Benevides <git@gui.dev.br>
2024-08-08 23:25:28 -0300

    fix: host issues

--- rules_test/index.ts
+++ rules_test/index.ts
@@ -6,13 +6,13 @@ import fs from "fs";
 import path from "path";
 import { runFirestoreTests } from "./tests/firestore";
 const firestoreRulesPath = path.resolve(__dirname, "..", "firestore.rules");
-
+const host = process.env.EMULATOR_HOST || "localhost";
 async function start() {
   let testEnv = await initializeTestEnvironment({
     projectId: "gui-dev-br",
     firestore: {
       rules: fs.readFileSync(firestoreRulesPath, "utf8"),
-      host: "localhost",
+      host,
       port: 8080,
     },
   });
@@ -22,7 +22,7 @@ async function start() {
   });
   // Firestore Rules Tests with Emulator
   const firestore = getFirestore();
-  connectFirestoreEmulator(firestore, "localhost", 8080);
+  connectFirestoreEmulator(firestore, host, 8080);
   await runFirestoreTests(firestore, testEnv);
   await testEnv.clearFirestore();
   process.exit(0);
