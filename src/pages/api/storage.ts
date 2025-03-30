7179acc3e492f4d80f784e19a3da82f0f366b617
Guilherme da Silva Benevides <git@gui.dev.br>
2025-03-30 20:18:00 -0300

    feat: update enviroment variables and url

--- src/pages/api/storage.ts
+++ src/pages/api/storage.ts
@@ -9,7 +9,9 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     const fileInfo = await getFileInfo(req.query.id);
     if (!fileInfo) return res.status(404).json({ error: "File not found" });
     if (!fileInfo.visible) return res.status(403).json({ error: "File not visible" });
-    return res.redirect("https://storage.function.gui.dev.br/" + req.query.id);
+    const isProduction = process.env.NODE_ENV !== "development";
+    if (isProduction) return res.redirect("https://storage.functions.gui.dev.br/" + req.query.id);
+    else return res.redirect("http://127.0.0.1:5001/gui-dev-br/us-central1/storage/" + req.query.id);
   } catch (error) {
     console.log("Api error", error);
     return res.status(500).json({ error: "Internal server error" });
