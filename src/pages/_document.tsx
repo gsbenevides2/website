ec800d23f5cda92d29c7495347b6998bf333af69
Guilherme da Silva Benevides <git@gui.dev.br>
2025-08-09 14:45:54 -0300

    update versions

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,45 +1,26 @@
-import Document, {
-  Html,
-  Head,
-  Main,
-  NextScript
-} from 'next/document'
+import { GoogleTagManager } from "@next/third-parties/google";
+import { Head, Html, Main, NextScript } from "next/document";
 
-export default class MyDocument extends Document {
+export default function Document() {
+  return (
+    <Html lang="pt-BR">
+      <Head>
+        <meta charSet="utf-8" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="preconnect" href="https://vitals.vercel-insights.com" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#7372a5" />
+        <meta name="msapplication-TileColor" content="#7372a5" />
+        <meta name="theme-color" content="#7372a5" />
+        <GoogleTagManager gtmId="GTM-NW5NDZP2" />
+      </Head>
 
-  render(): JSX.Element {
-    return (
-      <Html lang="pt-BR">
-        <Head>
-          <meta charSet="utf-8" />
-          <link
-            rel="apple-touch-icon"
-            sizes="180x180"
-            href="/apple-touch-icon.png"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="32x32"
-            href="/favicon-32x32.png"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="16x16"
-            href="/favicon-16x16.png"
-          />
-          <link rel="preconnect" href="https://vitals.vercel-insights.com" />
-          <link rel="manifest" href="/site.webmanifest" />
-          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#7372a5" />
-          <meta name="msapplication-TileColor" content="#7372a5" />
-          <meta name="theme-color" content="#7372a5" />
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    )
-  }
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
 }
